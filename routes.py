be2a1fb17ae2e1aca89e64d2fd35d5e452efe722
Emanuel Dionicio <emanuelbenjamin@hotmail.com>
2022-12-24 02:51:48 -0600

    avance en punto numero 4

--- routes.py
+++ routes.py
@@ -3,16 +3,19 @@ import json
 
 #entities
 from entietes import User
+from entietes import Ticket
 
 #models
 
 from models import MovieModel
 from models import UserModel
+from models import LoginModel
 
 
 movies_main = Blueprint('movie_blueprint', __name__)
 users_main = Blueprint('user_blueprint', __name__)
 login_main =  Blueprint('login_blueprint', __name__ )
+buy_ticket_main = Blueprint('buy_ticket', __name__)
 
 @movies_main.route('/')
 def get_movies():
@@ -42,15 +45,36 @@ def add_user():
 
 @login_main.route('/', methods = ['GET', 'POST'])
 def login():
-    
+
     try:
         email = request.json['email']
-        password = int(request.json['password'])
-        user=User("", "", "", password,email, "")
-        affected_rows = Login.login(user)
-        if affected_rows == 1:
-            return jsonify('Registered user')
+        password = request.json['password']
+
+        user=User("", "", "", password,"", "")
+        affected_rows2 = LoginModel.verify_password(user)
+        user=User("", "", "", "",email, "")
+        affected_rows1 = LoginModel.verify_email(user)
+
+        if affected_rows1 == 1 and affected_rows2 == 1:
+            #auth = HTTPBasicAuth('email', 'password')
+            #requests.post('https://postman-echo.com/basic-auth', auth=auth)
+            #response = requests.get('https://postman-echo.com/basic-auth', auth=auth)
+            #print(response.headers)
+            return jsonify('exito')
         else:
             return jsonify({'message': "User not created"}), 500
     except Exception as ex:
-        return jsonify({'message': "Invalid email or password"}), 
+        return jsonify({'message': str(ex)}), 500
+
+@buy_ticket_main.route('/',methods=['POST'])
+
+    
+def buy_ticket():
+    try:
+        id_movie = int(request.json['id_movie'])
+        seat_number = int(request.json['seat_number'])
+        id_showtime = int(request.json['id_showtime'])
+        ticket = Ticket("",)
+    except Exception as ex:
+        return jsonify({'message': str (ex)}), 500
+
